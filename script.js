b053dbc120211eb15917c13579652b67ab632d08
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2019-12-17 13:05:52 -0600

    added building and populating canvas grid from script

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-function reset() {
+const container = document.querySelector(".container");
+const grid = document.querySelector(".grid");
+
+let gridWidth = 10;
+let gridHeight = gridWidth;
+let gridSize = gridWidth * gridHeight;
+
+function resetGrid() {
+	for (i = 0; i < gridSize; i++) {
+		const gridCell = document.createElement('div');
+		gridCell.classList.add('gridCell');
+		grid.appendChild(gridCell);
+	}
 }
 
-function removeTransition(e) {
-	if (e.propertyName !== 'transform') return; // skip if it's not a transform
-	this.classList.remove('clicked');
-}
+resetGrid();
 
+//
+//grid.textContent = 'I\'m gonna be a grid!';
+//
+//function reset() {
+//}
+//
+//function removeTransition(e) {
+//	if (e.propertyName !== 'transform') return; // skip if it's not a transform
+//	this.classList.remove('clicked');
+//}
+//
