342ed95548d28a09aec9533b488e7572eb7fad01
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2019-12-17 15:35:43 -0600

    drawing in black works

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 const grid = document.querySelector(".grid");
 
-let gridWidth = 10;
+let gridWidth = 16;
 let gridHeight = gridWidth;
 let gridSize = gridWidth * gridHeight;
 
@@ -15,12 +15,19 @@ function resetGrid() {
 
 resetGrid();
 
+// add event listener to all grid cells to change bgcolor to black on hover
+const gridCells = document.querySelectorAll('.gridCell');
+gridCells.forEach(gridCell => gridCell.addEventListener('mouseenter', drawColor));
+
 //
 //grid.textContent = 'I\'m gonna be a grid!';
 //
 //function reset() {
 //}
 //
+function drawColor(e) {
+	this.style.background = '#000';
+}
 //function removeTransition(e) {
 //	if (e.propertyName !== 'transform') return; // skip if it's not a transform
 //	this.classList.remove('clicked');
